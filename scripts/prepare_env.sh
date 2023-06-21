1da91bf0fcc30513cc56f64e80fe11c211d994c1
aanorlondo <khaled.khebbeb@gmail.com>
2023-06-21 13:48:08 +0200

    fix: user docker host for databases

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -4,12 +4,18 @@
 export MYSQL_ROOT_PASSWORD=root
 
 # AUTH SERVER
-export DATABASE_HOSTNAME=$(hostname)
+export DATABASE_HOSTNAME=host.docker.internal #$(hostname)
 export DATABASE_PORT=3306
-export REDIS_HOSTNAME=$(hostname)
+export REDIS_HOSTNAME=host.docker.internal #$(hostname)
 export REDIS_PORT=6379
 export APP_SECRET_KEY=azertyuiop12345
 export API_ADMIN_TOKEN=admin
+
+# SWAGGER
+export HOSTNAME=$(hostname)
+input_swagger_file="../app/server/api/api_template.yaml"
+output_swagger_file="../app/server/api/api.yaml"
+envsubst < $input_swagger_file > $output_swagger_file
 
 # INTEGRATION
 # // app and mysql
